376891f71b19fbebc590ed237ee0348408c96f5c
Leandro Amaral <lramaral02m@gmail.com>
2024-03-28 15:27:29 -0300

    Adicionado busca por unidade federativa

--- src/app/shared/form-busca/dropdown-uf/dropdown-uf.component.ts
+++ src/app/shared/form-busca/dropdown-uf/dropdown-uf.component.ts
@@ -1,13 +1,45 @@
-import { Component, Input } from '@angular/core';
+import { UnidadeFederativa } from 'src/app/core/types/type';
+import { UnidadeFederativaService } from './../../../core/services/unidade-federativa.service';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { FormBuscaService } from 'src/app/core/services/form-busca.service';
+import { Observable, map, startWith } from 'rxjs';
 
 @Component({
   selector: 'app-dropdown-uf',
   templateUrl: './dropdown-uf.component.html',
   styleUrls: ['./dropdown-uf.component.scss']
 })
-export class DropdownUfComponent {
+export class DropdownUfComponent implements OnInit {
   @Input() label: string = '';
   @Input() iconePrefixo: string = '';
+  @Input() control!: FormControl;
 
-  filteredOptions = []
+  unidadesFederativas: UnidadeFederativa[] = [];
+
+  filteredOptions$?: Observable<UnidadeFederativa[]>;
+
+  constructor (private unidadeFederativaService: UnidadeFederativaService) {
+
+  }
+
+  ngOnInit(): void {
+    this.unidadeFederativaService.listar()
+      .subscribe(dados => {
+        this.unidadesFederativas = dados
+        console.log(this.unidadesFederativas);
+
+      })
+
+      this.filteredOptions$ = this.control.valueChanges.pipe(startWith(''),
+        map(value => this.filtrarUfs(value)))
+  }
+
+  filtrarUfs(value: string): UnidadeFederativa[] {
+    const valorFiltrado = value?.toLowerCase();
+    const result = this.unidadesFederativas.filter(
+      estado => estado.nome.toLowerCase().includes(valorFiltrado)
+    )
+    return result
+  }
 }
