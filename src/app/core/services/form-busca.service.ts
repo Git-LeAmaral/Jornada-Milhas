bd6ce80c71bf6dce89ecae8ce320f7e923676acc
Leandro Amaral <lramaral02m@gmail.com>
2024-03-29 15:07:36 -0300

    Criado funções de incremento e decremento

--- src/app/core/services/form-busca.service.ts
+++ src/app/core/services/form-busca.service.ts
@@ -17,8 +17,43 @@ export class FormBuscaService {
       somenteIda: new FormControl(false),
       origem: new FormControl(null),
       destino: new FormControl(null),
-      tipo: new FormControl("Econômica")
+      tipo: new FormControl("Econômica"),
+      adultos: new FormControl(1),
+      criancas: new FormControl(0),
+      bebes: new FormControl(0)
     });
+  }
+
+  getDescricaoPassageiros (): string {
+    let descricao = ''
+
+    const adultos = this.formBusca.get('adultos')?.value
+    if (adultos && adultos > 0) {
+      descricao += `${adultos} adulto ${adultos > 1 ? 's' : ''}`;
+    }
+
+    const criancas = this.formBusca.get('criancas')?.value
+    if (criancas && criancas > 0) {
+      descricao += `${descricao ? ', ' : ''} ${criancas} crianca ${criancas > 1 ? 's' : ''}`;
+    }
+
+    const bebes = this.formBusca.get('bebes')?.value
+    if (bebes && bebes > 0) {
+      descricao += `${descricao ? ', ' : ''} ${bebes} bebê ${bebes > 1 ? 's' : ''}`;
+    }
+
+    return descricao
+
+  }
+
+  trocarOrigemDestino(): void {
+    const origem = this.formBusca.get('origem')?.value;
+    const destino = this.formBusca.get('destino')?.value;
+
+    this.formBusca.patchValue({
+      origem: destino,
+      destino: origem
+    })
   }
 
   obterControle(nome:string): FormControl {
